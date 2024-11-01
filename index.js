c8e0434c38d33132d52ab3f4b6dd66fad8e9a25e
Yasminqilah <144866738+Yasminqilah@users.noreply.github.com>
2024-11-01 17:27:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,70 +3,70 @@
     "coffee_id": 1,
     "coffee_title": "Caffè Latte",
     "coffee_detail": "Rich full-bodied espresso in steamed milk lightly topped with foam.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/caffe_latte_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/caffe_latte_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/caffe_latte_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/caffe_latte_poster.jpg"
   },
   {
     "coffee_id": 2,
     "coffee_title": "Cappuccino",
     "coffee_detail": "Espresso with steamed milk topped with a deep layer of foam.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/cappuccino_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/cappuccino_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/cappuccino_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/cappuccino_poster.jpg"
   },
   {
     "coffee_id": 3,
     "coffee_title": "Caffè Mocha",
     "coffee_detail": "Espresso with bittersweet mocha sauce and steamed milk topped with sweetened whipped cream. Delightful.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/caffe_mocha_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/caffe_mocha_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/caffe_mocha_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/caffe_mocha_poster.jpg"
   },
   {
     "coffee_id": 4,
     "coffee_title": "Caramel Macchiato",
     "coffee_detail": "Espresso combined with vanilla-flavoured syrup, milk, and caramel sauce over ice.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/caramel_macchiato_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/caramel_macchiato_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/caramel_macchiato_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/caramel_macchiato_poster.jpg"
   },
   {
     "coffee_id": 5,
     "coffee_title": "Vanilla Sweet Cream Cold Brew",
     "coffee_detail": "Just before serving, our slow-steeped Cold Brew is topped with a delicate float of house-made vanilla sweet cream that cascades throughout the cup.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/vanilla_sweet_cream_cold_brew_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/vanilla_sweet_cream_cold_brew_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/vanilla_sweet_cream_cold_brew_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/vanilla_sweet_cream_cold_brew_poster.jpg"
   },
   {
     "coffee_id": 6,
     "coffee_title": "Cold Brew",
     "coffee_detail": "Slow-steeped, small-batch and super smooth.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/cold_brew_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/cold_brew_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/cold_brew_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/cold_brew_poster.jpg"
   },
   {
     "coffee_id": 7,
     "coffee_title": "Mango Passion Frappuccino",
     "coffee_detail": "A tropical mango and passionfruit infusion perfectly blended with a fruity hibiscus tea and ice.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/mango_passion_frappuccino_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/mango_passion_frappuccino_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/mango_passion_frappuccino_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/mango_passion_frappuccino_poster.jpg"
   },
   {
     "coffee_id": 8,
     "coffee_title": "Raspberry Currant Frappuccino",
     "coffee_detail": "Made with freshly brewed Teavana® tea blended juice and ice.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/raspberry_currant_frappuccino_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/raspberry_currant_frappuccino_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/raspberry_currant_frappuccino_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/raspberry_currant_frappuccino_poster.jpg"
   },
   {
     "coffee_id": 9,
     "coffee_title": "Coffee Frappuccino",
     "coffee_detail": "Starbucks® coffee is blended with nonfat milk and ice for a sip on the light side.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/coffee_frappuccino_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/coffee_frappuccino_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/coffee_frappuccino_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/coffee_frappuccino_poster.jpg"
   },
   {
     "coffee_id": 10,
     "coffee_title": "Mocha Frappuccino",
     "coffee_detail": "Mocha sauce, Frappuccino® roast coffee, milk and ice all come together for a mocha flavor that'll leave you wanting more.",
-    "thumbnail": "https://2202983-uts-mopro.vercel.app/assets/mocha_frappuccino_thumbnail.png",
-    "poster": "https://2202983-uts-mopro.vercel.app/assets/mocha_frappuccino_poster.jpg"
+    "thumbnail": "https://2202983-uts-mopro.vercel.app/mocha_frappuccino_thumbnail.png",
+    "poster": "https://2202983-uts-mopro.vercel.app/mocha_frappuccino_poster.jpg"
   }
 ]
